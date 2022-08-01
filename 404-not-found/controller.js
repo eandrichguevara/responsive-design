913b1e91744d2a98cee34342c7a6f8a75a85e96d
Emilio Andrich <eandrichguevara@gmail.com>
2022-08-01 12:28:06 -0400

    Reorder and Add button Change Style

---
+++ 404-not-found/controller.js
@@ -0,0 +1,23 @@
+var lastStyle = 0;
+const styles = [
+    './styles/style-amico.css',
+    './styles/style-bro.css',
+    './styles/style-cuate.css',
+    './styles/style-pana.css',
+    './styles/style-rafiki.css'
+];
+
+var head = document.getElementsByTagName('HEAD')[0]; 
+
+var link = document.createElement('link');
+
+link.rel = 'stylesheet'; 
+link.type = 'text/css';
+link.href = styles[lastStyle]; 
+
+head.appendChild(link);
+
+function changeStyle(){
+    lastStyle++;
+    link.href = styles[lastStyle];
+}
